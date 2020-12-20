7823dbd2289052898e8aa17cd530b9c568f82db9
Rohit Khurana <khuranarohit75@gmail.com>
2020-12-19 19:03:03 -0700

    improved data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -19,28 +19,25 @@ def create_emotions_df(input_path):
 	# read in and clean emotions dataset
 	emotions_df = pd.read_csv(input_path, skiprows = 27, names = features, 
 		skipfooter = 2, engine = "python")
-
-	emotions_df.reset_index(inplace = True)
-	emotions_df = (emotions_df.set_index("level_1").rename_axis("Milliseconds"))
-	emotions_df.drop(emotions_df.columns[0:9], inplace = True, axis = 1)
+	emotions_df = emotions_df.reset_index().set_index("level_1").rename_axis("Milliseconds")
+	emotions_df.drop(columns = emotions_df.columns[0:9], inplace = True)
 
 	return emotions_df
 
-def plot_emotions(emotions_df, output_path):
-	'''plots data from the emotions dataframe'''
+def plot_emotions(emotions_df, output_path = None):
 	prev_feature = 0
 
 	# slice the dataframe every two columns and plot on a new figure 
 	# until all columns are utilized
 	for curr_feature in range(2, len(features) + 1, 2):
-		fig, [ax1, ax2] = plt.subplots(nrows = 2, ncols = 1, figsize = (18, 8))
+		fig, ((ax1), (ax2)) = plt.subplots(nrows = 2, ncols = 1, figsize = (18, 8))
 
 		sliced_df = emotions_df.iloc[:, prev_feature:curr_feature]
 		prev_feature = curr_feature
 
 		sliced_df.plot(subplots = True, color = {"green", "orange"}, 
 			title = list(sliced_df.columns), legend = False, sharex = False, 
-			ax = [ax1, ax2])
+			ax = (ax1, ax2))
 
 		ax1.set_xlabel("Milliseconds", labelpad = 3)
 		ax2.set_xlabel("Milliseconds", labelpad = 3)
@@ -50,36 +47,37 @@ def plot_emotions(emotions_df, output_path):
 		
 		plt.tight_layout(pad = 3)
 
-		fig.savefig(output_path + "/" + str(sliced_df.columns) + ".pdf")
-		#plt.show()
-	
+		#fig.savefig(output_path + "/" + str(sliced_df.columns) + ".pdf")
+		plt.show()
+
 def create_power_hr_df(hr_input_path, pwr_input_path):
-	# load heart rate df
 	heart_rate_df = pd.read_csv(hr_input_path, sep = "-", 
-		names = ["capture", "HR"])
-	heart_rate_df["capture"] = heart_rate_df["capture"].str.extract(r'(\d{1,4})')
+		header = None, names = ["File", "HR"]).rename_axis("seconds")
+	heart_rate_df.drop(columns = ["File"], inplace = True)
 
-	# load power df
 	power_df = pd.read_csv(pwr_input_path, sep = "-", 
-		names = ["capture", "Power"])
-	power_df["capture"] = power_df["capture"].str.extract(r'(\d{1,4})')
+		header = None, names = ["File", "Power"]).rename_axis("seconds")
+	power_df.drop(columns = ["File"], inplace = True)
 
-	# merge power and heart rate together
-	merged_df = (heart_rate_df.merge(power_df, on = "capture").
-		set_index("capture").rename_axis("seconds"))
+	merged_df = (heart_rate_df.merge(power_df, on = "seconds"))
 	return merged_df
 
 def clean_power_hr_df(hr_input_path, pwr_input_path):
 	merged_df = create_power_hr_df(hr_input_path, pwr_input_path)
 
-	# get rid of any unnecessary whitespace including between digits
+	# regex expression adopted from this post
+	# https://www.journaldev.com/23763/python-remove-spaces-from-string
 	merged_df.replace(r'^\s*$', np.nan, inplace = True, regex = True)
+
+	# regex expression adopted from this forum
+	# https://stackoverflow.com/questions/38640791/remove-spaces-between-numbers-in-a-string-in-python
 	merged_df.replace(r'\s*(\d)\s+(\d)', r'\1\2', inplace = True, regex = True)
 
 	merged_df = merged_df.apply(pd.to_numeric).dropna(axis = 0)
 
 	# implement z score to detect outliers in the tesseract dataset 
-	# (code derived from TDS online Medium publication, author: Natasha Sharma) 
+	# (code derived from TDS online Medium publication, author: Natasha Sharma)
+	# https://towardsdatascience.com/ways-to-detect-and-remove-the-outliers-404d16608dba 
 	z = np.abs(stats.zscore(merged_df))
 	cleaned_df = merged_df[(z < 3).all(axis = 1)]
 
@@ -87,10 +85,10 @@ def clean_power_hr_df(hr_input_path, pwr_input_path):
 	pd.options.mode.chained_assignment = None
 	cleaned_df["ewm_HR"] = cleaned_df["HR"].ewm(alpha = 0.1).mean()
 	cleaned_df["ewm_Power"] = cleaned_df["Power"].ewm(alpha = 0.1).mean()
-
+	
 	return cleaned_df
 
-def plot_tesseract_results(cleaned_df, output_path):
+def plot_tesseract_results(cleaned_df, output_path = None):
 	'''plots data from the tesseract dataframe'''
 	fig, [ax1, ax2] = plt.subplots(nrows = 2, ncols = 1, figsize = (18, 8))
 
@@ -108,7 +106,7 @@ def plot_tesseract_results(cleaned_df, output_path):
 	plt.tight_layout(pad = 3) # increase spacing between subplots
 	plt.tick_params(axis = "x", rotation = 0)
 
-	fig.suptitle("Seuil 30", x = 0.5155, y = 0.99)
+	fig.suptitle("Title", x = 0.5155, y = 0.99)
 	#fig.savefig(output_path + "/tesseract_plot.pdf")
 	plt.show()
 
@@ -118,14 +116,14 @@ def generate_subject(emotions_df, power_hr_df):
 	emotions = emotions_df.iloc[:, :10]
 	emotions.index = (np.array(emotions.index) / 60000).astype(int)
 	new_emotions_df = (emotions.groupby(emotions.index).mean().
-		rename_axis("group_secs"))
+		rename_axis("minutes"))
 
 	power_hr_df.index = power_hr_df.index.astype(int)
 	power_hr_df.index = (np.array(power_hr_df.index) / 60).astype(int)
 	new_pwr_df = (power_hr_df.groupby(power_hr_df.index).mean().
-		drop(["ewm_HR", "ewm_Power"], axis = 1).rename_axis("group_secs"))
+		drop(["ewm_HR", "ewm_Power"], axis = 1).rename_axis("minutes"))
 
-	subject = new_pwr_df.merge(new_emotions_df, on = "group_secs")
+	subject = new_pwr_df.merge(new_emotions_df, on = "minutes")
 	return subject
 
 def generate_scatter_plots(subject):
@@ -164,26 +162,23 @@ def even_more_plots(subject):
 	sns.regplot(subject["Valence"], subject["Power"], color = "black")
 	plt.xlabel("Valence", labelpad = 7)
 	plt.ylabel("Power (W)", labelpad = 7)
-	plt.text(0.9, 0.05, "r = {:.3f}".format(correlation), transform = plt.gca().transAxes, 
-		horizontalalignment = "center", verticalalignment = "center", style = "italic")
-	plt.suptitle("Seuil 30: Valence vs. Power (1 min increments)", 
-		x = 0.5155, y = 0.95)
+	plt.suptitle("Title", x = 0.5155, y = 0.95)
 
 	# generate heat map of the pearson correlations
 	plt.figure(2, figsize = (8, 7))
 	plt.subplots_adjust(left = 0.125, right = 0.9, bottom = 0.16, top = 0.9, 
 		wspace = 0.2, hspace = 0.4)
 	sns.heatmap(pearson_correlations)
-	plt.suptitle("Seuil 30: Pearson Correlations", 
-		x = 0.5155, y = 0.95)
+	plt.suptitle("Title", x = 0.5155, y = 0.95)
 
 	# generate plot of engagement and attention over time
 	plt.figure(3)
-	subject[["Engagement", "Attention"]].plot(color = {"green", "black"})
+	subject["Engagement"].plot(color = "green")
+	subject["Attention"].plot(color = "black")
+	plt.legend()
 	plt.xlabel("Minutes", labelpad = 7)
 	plt.ylabel("FEA Metric", labelpad = 7)
-	plt.suptitle("Seuil 30: Engagement/Attention", 
-		x = 0.5155, y = 0.95)
+	plt.suptitle("Title", x = 0.5155, y = 0.95)
 
 	plt.show()
 
@@ -192,5 +187,5 @@ def percentage_video_captured(orig_df, mod_df):
 
 def main():
 	em_input_path = None
-	pwr_input_path = None
 	hr_input_path = None
+	pwr_input_path = None
